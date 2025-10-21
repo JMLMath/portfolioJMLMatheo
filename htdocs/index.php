ecbb9ab5c137ed5cb2f501798cc73ab00f0ffb37
JMLMath <jmlmatheo@icloud.com>
2025-10-21 19:12:54 +0200

    Added a new home page.

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,42 +9,26 @@ catch (Exception $e)
 {
     die('ERREUR : ' . $e->getMessage()); // arrete execution du programme
 }
+
+$requeteStr = 'SELECT * FROM projet;';
+$requete = $psqlclient->prepare($requeteStr);
+$requete->execute();
+$projets = $requete->fetchAll();
+
 ?>
 <html>
     <head>
         <title> Portfolio JML Mathéo </title>
         <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/head.php"; ?>
+        <link rel="stylesheet" href="/styles/style_first_page.css"/>
     </head>
     <body>
         <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"; ?>
 
-        <article>
-            <h2>Mes projets</h2>
-            <p> Voici quelques uns de mes projets, ci dessous. </p>
-            
-            <div class="projets">
-                <?php
-                $requeteStr = 'SELECT * FROM projet;';
-                $requete = $psqlclient->prepare($requeteStr);
-                $requete->execute();
-                $projets = $requete->fetchAll();
-                foreach ($projets as $projet)
-                {
-                    $projet_page_link = '/projet.php?id=' . $projet['id_projet'];
-                ?>
-                    <section>
-                        <h3> <?php echo $projet['intitule']; ?> </h3>
-                        <p> <?php echo $projet['objectif']; ?> </p>
-                        <p> <a href="<?php echo $projet_page_link;?>"> Pour en voir plus, cliquez ici. </a></p>
-                    </section>
-                <?php
-                }
-                ?>
-            </div>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/static_docs/title_page.html"; ?>
+        <h1>Qui suis-je ?</h1>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/static_docs/personal_presentation.html"; ?>
 
-
-            </article>
-
-            <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
     </body>
 </html>
